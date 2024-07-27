054186cce999f38ff97df2290fca2310499d1d81
ponces95 <129859289+ponces95@users.noreply.github.com>
2024-07-27 11:50:44 -0600

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,11 +3,21 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+const axios = require('axios');
 
 
 public_users.post("/register", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { username, password} = req.body;
+  if (!username || password) {
+    return res.status(400).json({message: 'Provide username and password'});
+  }
+  const userExists = users.find(user => user.username === username);
+  if (userExists) {
+    return res.status(409).json({message: 'User already exists'});
+  }
+  users.push({username, password});
+  return res.status(200).json({message: "User registered"});
 });
 
 // Get the book list available in the shop
@@ -30,14 +40,26 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let title = req.params.title;
+  let booksList = Object.values(books)
+  let book= booksList.find (b=>b.title === title);
+  if (book){
+    let bookDetails = JSON.stringfy(book);
+    res.send(`${title} details : ${bookDetails}`);
+  }else{
+    res.send ( `${title} not found`);}
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  let booksList = Object.values(books)
+  let book = booksList.find(b => b.isbn===isbn);
+  if (book){
+    const reviews = book.reviews;
+    res.send(reviews);
+  }else{
+    res.send('Book not found');}
 });
 
 module.exports.general = public_users;
